99bc66ac7be7708cd06f0deeef62fa64153f99fa
David Freitas <dvdfreitas@gmail.com>
2023-10-10 09:55:46 +0100

    231010 09:55

--- app/Console/Commands/ImportCSV.php
+++ app/Console/Commands/ImportCSV.php
@@ -4,9 +4,23 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Expr\Cast\Bool_;
 
 class ImportCSV extends Command
 {
+    /**
+     * 
+     */
+    public $validation = [
+        'projects' => [
+            'mandatory' => ['slug', 'name']
+        ],
+        'teachers' => [
+            'mandatory' => ['name']
+        ],
+    ];
+
+
     /**
      * The name and signature of the console command.
      *
@@ -35,20 +49,22 @@ class ImportCSV extends Command
         
         $validHeader = $this->validateHeader($header);
 
-        while(($line_csv = fgets($file)) !== false) {
-            $line = str_getcsv($line_csv, ";");
-            // $csv_id = $line[0];
-            // $this->info($csv_id);
-        }
+        // while(($line_csv = fgets($file)) !== false) {
+        //     $line = str_getcsv($line_csv, ";");
+        //     // $csv_id = $line[0];
+        //     // $this->info($csv_id);
+        // }
         
         fclose($file);
 
         return Command::SUCCESS;
     }
 
+    /**
+     * 
+     */
     public function verifyExistence($mandatory, $header) {
-        $result = true;
-        print_r($header);
+        $result = true;        
         foreach ($mandatory as $field) {
             if (!in_array($field, $header)) {
                 $result = false;
@@ -58,20 +74,29 @@ class ImportCSV extends Command
         return $result;
     }
 
-    public function validateHeader($header)
-    {        
-        
-        $validHeader = true;
-        if ($header[0] == "projects") {
-            $validHeader = $this->verifyExistence(["slug", "name", "description"], $header);
+    public function validTable($table) {
+        if (!array_key_exists($table, $this->validation)) {
+            $this->error("Table " . $table . " doesn't exist.");
+            return false;
         }
+        return true;
+    }
 
-        if ($validHeader)
-            dd("Está válido"); 
+
+    public function validateHeader($header)
+    {                
+        $validHeader = true;
+
+        // Check first if $header[0] is in validation array and not an empty key
+        if (array_key_exists($header[0], $this->validation))
+            $this->validTable($header[0]);
         else
-            dd("Inválido");
+            dd("Table " . $header[0] . " doesn't exist.");
+        $mandatory = $this->validation[$header[0]]['mandatory'];
         
-
+        $validHeader = $this->verifyExistence($mandatory, $header);
+        dd($validHeader);
+        
         // $line = $this->line($header);
 
         // foreach ($header as $key => $column) {                
